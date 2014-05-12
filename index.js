01c2535d3361cf31da20d8bc760c814ab107d023
Graham Melcher <graham@hint.com>
2014-05-12 09:40:49 -0700

    Support haml-coffee as well.

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-var haml = require('haml');
 
 var createHamlPreprocessor = function(args, config, logger, helper) {
+  var haml = require('haml');
   config = config || {};
   var log = logger.create('preprocessor.haml');
 
   return function(content, file, done) {
     log.debug('Processing "%s".', file.originalPath);
-    done(haml.compile(content))
+    file.path = file.path.replace('.haml', '.html');
+    done(haml.compile(content)
+  };
+};
+
+var createHamlCoffeePreprocessor = function(args, config, logger, helper) {
+  var hamlc = require('haml-coffee');
+  config = config || {};
+  var log = logger.create('preprocessor.haml');
+
+  return function(content, file, done) {
+    log.debug('Processing "%s".', file.originalPath);
+    file.path = file.path.replace('.haml', '.html');
+    done(hamlc.compile(content)())
   };
 };
 
 createHamlPreprocessor.$inject = ['args', 'config', 'logger', 'helper'];
+createHamlCoffeePreprocessor.$inject = ['args', 'config', 'logger', 'helper'];
 
 // PUBLISH DI MODULE
 module.exports = {
   'preprocessor:haml': ['factory', createHamlPreprocessor]
+  'preprocessor:haml-coffee': ['factory', createHamlCoffeePreprocessor]
 };
+
+
+
